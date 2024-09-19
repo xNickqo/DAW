1bc0d0d515fc848b77142dbd21cae6b66b5afcb5
xNickqo <nikolaslf007@gmail.com>
2024-09-19 13:14:00 +0200

    ipToBinary php

--- DWES/UT2_01 Strings/Ejercicio1.php
+++ DWES/UT2_01 Strings/Ejercicio1.php
@@ -2,7 +2,31 @@
 <HEAD><TITLE> EJ1-Conversion IP Decimal a Binario </TITLE></HEAD>
 <BODY>
 <?php
-    $ip="192.18.16.204";
+
+$ip = "192.18.16.204";
+
+function ipToBinary($ip)
+{
+    $substrs = 0;
+    $len = strlen($ip);
+    $i = 0;
+
+    while ($i < $len)
+    {
+        if ($ip[$i] == '.')
+        {
+            printf('%08b.', (int)$substrs);
+            $substrs = 0;
+        }
+        $substrs = $substrs * 10 + (int)$ip[$i];
+        $i++;
+    }
+    printf('%08b.', (int)$substrs);
+}
+print ("<p>IP: <b>$ip</b></p>\n");
+print ("IP en binario: ");
+ipToBinary($ip);
+
 ?>
 </BODY>
 </HTML>
