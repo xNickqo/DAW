932f6745689c445d8be38068ecaf8d5c0148168a
xNickqo <nikolaslf007@gmail.com>
2024-10-14 11:06:18 +0200

    corrigiendo

--- (DWES) Desarrollo Web Entorno Servidor/UT2_04 Formularios/Ej1f/fcalculadora.php
+++ (DWES) Desarrollo Web Entorno Servidor/UT2_04 Formularios/Ej1f/fcalculadora.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+        Operando 1: <input type="text" name="op1" value="' . htmlspecialchars($op1) . '"><br>
+        Operando 2: <input type="text" name="op2" value="' . htmlspecialchars($op2) . '"><br>
+        Selecciona operación: <br>
+        <input type="radio" name="op" value="suma"> Suma <br>
+        <input type="radio" name="op" value="resta"> Resta <br>
+        <input type="radio" name="op" value="div"> División <br>
+        <input type="radio" name="op" value="mult"> Multiplicación <br>
+        <input type="submit" value="Enviar">
+        <input type="reset" value="Borrar">
+    </form>
+</body>
+</html>
+
 <?php
     function procesarOperacion($op1, $op2, $operacion)
     {
@@ -52,18 +74,6 @@
 
         list($resultado, $signo) = procesarOperacion($op1, $op2, $_POST["op"]);
     }
-
-    echo '<form action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '" method="POST">
-            Operando 1: <input type="text" name="op1" value="' . htmlspecialchars($op1) . '"><br>
-            Operando 2: <input type="text" name="op2" value="' . htmlspecialchars($op2) . '"><br>
-            Selecciona operación: <br>
-            <input type="radio" name="op" value="suma"> Suma <br>
-            <input type="radio" name="op" value="resta"> Resta <br>
-            <input type="radio" name="op" value="div"> División <br>
-            <input type="radio" name="op" value="mult"> Multiplicación <br>
-            <input type="submit" value="Enviar">
-            <input type="reset" value="Borrar">
-        </form>';
 
     if (!empty($resultado) && is_numeric($resultado))
         echo "Resultado operación: $op1 $signo $op2 = $resultado";
--- (DWES) Desarrollo Web Entorno Servidor/UT2_05 Ficheros/fichero1.php
+++ (DWES) Desarrollo Web Entorno Servidor/UT2_05 Ficheros/fichero1.php
@@ -1,35 +1,52 @@
-<?php
-
-echo '<form action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '" method="POST">
-            Nombre: <input type="text" name="nombre" placeholder="Nombre" required><br>
-            Apellido 1: <input type="text" name="apellido1" placeholder="Apellido 1" required><br>
-            Apellido 2: <input type="text" name="apellido2" placeholder="Apellido 2" required><br>
-            Fecha de Nacimiento: <input type="date" name="fecha" required><br>
-            Localidad: <input type="text" name="localidad" placeholder="Localidad" required><br>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ficheros</title>
+</head>
+<body>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
+            Nombre: 
+            <input type="text" name="nombre" placeholder="Nombre" required><br>
+            Apellido 1: 
+            <input type="text" name="apellido1" placeholder="Apellido 1" required><br>
+            Apellido 2:
+            <input type="text" name="apellido2" placeholder="Apellido 2" required><br>
+            Fecha de Nacimiento:
+            <input type="date" name="fecha" required><br>
+            Localidad:
+            <input type="text" name="localidad" placeholder="Localidad" required><br>
+            
             <br>
+
             <input type="submit" value="Enviar">
             <input type="reset" value="Borrar">
-        </form>';
+        </form>
+
+</body>
+</html>
+
+<?php
+// Función para escribir un campo carácter por carácter en el archivo
+function escribir_campo($archivo, $campo, $longitud)
+{
+    // Escribir cada carácter del campo hasta el máximo permitido
+    for ($i = 0; $i < $longitud; $i++)
+    {
+        if ($i < strlen($campo))
+        {
+            fwrite($archivo, $campo[$i]);
+        }
+        else
+        {
+            fwrite($archivo, " ");
+        }
+    }
+}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-    // Función para escribir un campo carácter por carácter en el archivo
-    function escribir_campo($archivo, $campo, $longitud)
-    {
-        // Escribir cada carácter del campo hasta el máximo permitido
-        for ($i = 0; $i < $longitud; $i++)
-        {
-            if ($i < strlen($campo))
-            {
-                fwrite($archivo, $campo[$i]);
-            }
-            else
-            {
-                fwrite($archivo, " ");
-            }
-        }
-    }
-
     $archivo = fopen("alumnos1.txt", "a");
 
     if ($archivo)
