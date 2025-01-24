6cdaabe5f090189096e44e6ffc9a07fe5a0367ca
xNickqo <nikolaslf007@gmail.com>
2025-01-24 12:02:56 +0100

    ddd

---
+++ DWEC/Practica7_01AJAX/cantabria.js
@@ -0,0 +1,13 @@
+if (document.addEventListener)
+	window.addEventListener("load",iniciar)
+else if (document.attachEvent)
+	window.attachEvent("onload",iniciar);
+
+
+function iniciar(){
+	if (document.addEventListener){
+
+	} else if (document.attachEvent){
+		
+	}
+}
